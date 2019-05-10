3787e8dee601712787a98aafa0af3f8ca3519cd9
aaronchu415 <aaronchu415@gmail.com>
2019-05-10 14:58:19 -0700

    added geeks for geeks

--- Classes/InterviewQuestion.py
+++ Classes/InterviewQuestion.py
@@ -6,19 +6,24 @@ class InterviewQSearcher():
         
     def getLinks(self,search_word):
 
+        ##Links from AD
         URLS = ['http://www.ardendertat.com/2011/','http://www.ardendertat.com/2011/page/2/','http://www.ardendertat.com/2011/page/3/'] 
 
         resultsArr = []
 
         for url in URLS:
-            links = self.__getLinksFromPage(url,search_word)
+            links = self.__getLinksFromAD(url,search_word)
             resultsArr.append(links)
 
+        ##links from GG
+        gg_links = self.__getLinksFromGG(search_word)
+        resultsArr.append(gg_links)
+
+        ## Flatten list
         flat_list = [item for sublist in resultsArr for item in sublist]
-  
         return flat_list
 
-    def __getLinksFromPage(self,pageUrl,search_word):
+    def __getLinksFromAD(self,pageUrl,search_word):
 
         response = requests.get(pageUrl)
         soup = bs4.BeautifulSoup(response.text, features='html5lib')
@@ -31,7 +36,30 @@ class InterviewQSearcher():
 
         return links
 
+    def __getLinksFromGG(self,search_word):
+        URL = 'https://www.geeksforgeeks.org/fundamentals-of-algorithms/'
+
+        response = requests.get(URL)
+        soup = bs4.BeautifulSoup(response.text, features='html5lib')
+        ol_tags = soup.find_all('ol')
+
+        total_links = []
+
+        for ol_tag in ol_tags:
+            li_tags = ol_tag.find_all('li')
+            li_filtered = [li for li in li_tags if search_word in li.text.lower()]
+            links = [[li.text, li.findChildren('a')[0]['href']] for li in li_filtered]
+
+            if links:
+                total_links.append(links)
+
+        flat_list = [item for sublist in total_links for item in sublist]
+        return flat_list
+
 
 # iqs = InterviewQSearcher()
-# print(iqs.getLinks('tree'))
+# results = iqs.getLinks('tree')
+# for links in results:
+#     print('\n')
+#     print(links)
 
