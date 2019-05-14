f9d4369398cd3fcc10ceba8f58dd850bcceb1136
odoland <orlando.wong@yahoo.com>
2019-05-13 22:42:34 -0700

    switched to aiohttp for non-blocking async get requests for WordSearcher.py class

--- Classes/WordSearch.py
+++ Classes/WordSearch.py
@@ -1,7 +1,7 @@
 import requests
 import bs4
 import asyncio
-# from fuzzywuzzy import fuzz
+import aiohttp
 
 COHORTS = ['r11', 'r10', 'r9', 'r8']
 BASE_URL = "http://curric.rithmschool.com/{cohort}/lectures/"
@@ -15,9 +15,10 @@ class WordSearcher:
         base_url is the link to the main page that contains all the lecture links"""
         
         self.base_url = base_url
-        self.links = self._get_lecture_links()
+        self.links = self._get_lecture_links_from_table_of_contents()
 
     def get_results(self, word, COLLECTION_NAME):
+        """Get all search results based on collection name with the word """
 
         switcher = { 
             "links": self._get_links_with_word, 
@@ -29,7 +30,7 @@ class WordSearcher:
         return asyncio.run(switcher.get(COLLECTION_NAME, lambda x : [])(word))
     
     # PRIVATE METHOD
-    def _get_lecture_links(self):
+    def _get_lecture_links_from_table_of_contents(self):
         """Returns all the lecture links as an array of strings """
         
         # Go the the main links page
@@ -42,6 +43,12 @@ class WordSearcher:
 
         return links
 
+    async def fetch(self, session, url):
+        """Asynchronous get request to the url """
+        async with session.get(url) as response:
+            return await response.text(encoding="utf-8")
+            
+
     # PRIVATE METHOD
     async def _search_link_word(self, link: str, word: str, option):
         """Searches one link for the word. If it is found, returns the link, else returns None 
@@ -51,36 +58,38 @@ class WordSearcher:
         3) lecture pages - gets full lectures pages
         """
         
-        html_text = self.get_link_html(link)
-
-        soup = bs4.BeautifulSoup(html_text, features='html5lib')
-
-        if option == "links": # returns (https://curric.rithmschool.com/lectures/etc/)
-            if word.lower() in soup.text.lower():
-                return (f"{self.base_url}{link}")
+        async with aiohttp.ClientSession() as session:
+            full_url = self.base_url + link
+            html = await self.fetch(session, full_url)
+            soup = bs4.BeautifulSoup(html, features='html5lib')
         
-        elif option == "code_snips":  # returns (https://curric.rithmschool.com/lectures/etc/, <pre> codesnip </pre>)
-            pre = soup.find_all('pre')   
+            if option == "links": # returns (https://curric.rithmschool.com/lectures/etc/)
+                if word.lower() in soup.text.lower():
+                    return (f"{self.base_url}{link}")
             
-            for code_snip in pre:
-                if word.lower() in code_snip.text.lower():  
-                    return f"{self.base_url}{link}", code_snip.text
-        
-        elif option == "lecture_pages": # Returns full HTML of the lecture page <html> <html>
-            if word.lower() in soup.text.lower():
-
-                full_link = f"{self.base_url}{link}"
-                body = soup.find('body')
-                img_tags = body.find_all('img')
-
-                # replace img tag with full link
-                for img_tag in img_tags:
-                    fixed_image_link = f"{full_link}{img_tag['src']}"
-                    img_tag['src'] = img_tag['src'].replace(img_tag['src'], fixed_image_link)
+            elif option == "code_snips":  # returns (https://curric.rithmschool.com/lectures/etc/, <pre> codesnip </pre>)
+                pre = soup.find_all('pre')   
                 
-                return full_link, str(body)
-        # Nothing was found
-        return None
+                for code_snip in pre:
+                    if word.lower() in code_snip.text.lower():  
+                        print("success! found word in code_snip! for", link)
+                        return f"{self.base_url}{link}", code_snip.text
+            
+            elif option == "lecture_pages": # Returns full HTML of the lecture page <html> <html>
+                if word.lower() in soup.text.lower():
+
+                    full_link = f"{self.base_url}{link}"
+                    body = soup.find('body')
+                    img_tags = body.find_all('img')
+
+                    # replace img tag with full link
+                    for img_tag in img_tags:
+                        fixed_image_link = f"{full_link}{img_tag['src']}"
+                        img_tag['src'] = img_tag['src'].replace(img_tag['src'], fixed_image_link)
+                    
+                    return full_link, str(body)
+            # Nothing was found
+            return None
 
     async def _get_links_with_word(self, word: str):
         """Returns a list of all the links with that word. 
@@ -114,7 +123,7 @@ class WordSearcher:
         return results
  
         
-    def get_link_html(self, link : str):
+    async def get_link_html(self, link : str):
         """Returns the html text from the given link.
         If the html for that text is not stored in the html folder, issue a get request and store it.
         Else read it from the html file storage.
@@ -145,6 +154,6 @@ class WordSearcher:
 if __name__ == "__main__":
         
     wc = WordSearcher("http://curric.rithmschool.com/r11/lectures/")
-    s = asyncio.run(wc._get_lecture_pages("flask"))
+    s = asyncio.run(wc._get_links_with_word("flask"))
     print(s)
 
