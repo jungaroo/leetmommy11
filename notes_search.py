c6ec27c2351d601d8299789cee2190fcfbc22e02
odoland <orlando.wong@yahoo.com>
2019-05-13 22:32:41 -0700

    switched to async aiohttp for notes_search

--- notes_search.py
+++ notes_search.py
@@ -4,19 +4,27 @@ import re
 import argparse
 import sys
 import asyncio
-
+import aiohttp
 
 BASE_URL = "http://curric.rithmschool.com/r11/lectures/"
 
-async def search_links(link, word):
-    """ Asynchronously searches for word in the link """
-    response = requests.get(BASE_URL + link)
-    soup = bs4.BeautifulSoup(response.text, features='html5lib')
-    if word in soup.text: 
-        print("Try checking in:", f"{BASE_URL}{link}")
+async def fetch(session, url):
+    """Asynchronous get request to the url """
+    async with session.get(url) as response:
+        return await response.text(encoding="utf-8")
+      
 
-async def get_all_links():
-    """ Asynchronously searches for all links """
+async def search_links(link, word):
+    """Asynchronous get request to url """
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, BASE_URL + link)
+        soup = bs4.BeautifulSoup(html, features='html5lib')
+        if word in soup.text:
+            print("Try checking in:", BASE_URL + link)
+
+
+def get_all_links():
+    """ Searches for all links """
     response = requests.get(BASE_URL)
     soup = bs4.BeautifulSoup(response.text, features='html5lib')
 
@@ -50,7 +58,7 @@ async def main():
 
 
     # Grab all links
-    links = await get_all_links()
+    links = get_all_links()
 
     # search for the word
     await asyncio.gather(*[search_links(link, word) for link in links])
